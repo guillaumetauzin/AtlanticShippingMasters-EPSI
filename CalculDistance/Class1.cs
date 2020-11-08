b54e8e4cc73407e17a23e809ff5c7fb830836ab2
matthieu pacheco <mattpacheco33@gmail.com>
2020-11-08 17:50:06 +0100

    Création du calcul de la distance sans gestion de l'erreur

---
+++ CalculDistance/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace CalculDistance
+{
+    class Class1
+    {
+        public double Calcul(double lat1, double lat2, double long1, double long2)
+        {
+            var R = 6371e3;
+            var φ1 = lat1 * Math.PI / 180;
+            var φ2 = lat2 * Math.PI / 180;
+            var Δφ = (lat2 - lat1) * Math.PI / 180;
+            var Δλ = (long2 - long1) * Math.PI / 180;
+
+            var a = Math.Sin(Δφ / 2) * Math.Sin(Δφ / 2) +
+                      Math.Cos(φ1) * Math.Cos(φ2) *
+                      Math.Sin(Δλ / 2) * Math.Sin(Δλ / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            var d = Math.Round(R * c / 1000);
+
+            return d;
+        }
+
+        public double ConversionLatitude(double degree, int minute, int seconde, string NS)
+        {
+            if (NS == "S")
+            {
+                degree = -degree;
+            }
+            double lati = degree + 1 / 60 * minute + 1 / 3600 * seconde;
+            return lati;
+        }
+
+        public double ConversionLongitude(double degree, int minute, int seconde, string OW)
+        {
+            if (OW == "W")
+            {
+                degree = -degree;
+            }
+            double longi = degree + 1 / 60 * minute + 1 / 3600 * seconde;
+            return longi;
+        }
+    }
+}
+
---
+++ CalculDistance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalculDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+            double lat1 = a.ConversionLatitude(59, 23, 23, "N");
+            double lat2 = a.ConversionLatitude(39, 22, 12, "N");
+            double long1 = a.ConversionLatitude(49, 12, 57, "W");
+            double long2 = a.ConversionLatitude(69, 34, 15, "W");
+
+            Console.WriteLine(a.Calcul(lat1, lat2, long1, long2));
+        }
+    }
+}
