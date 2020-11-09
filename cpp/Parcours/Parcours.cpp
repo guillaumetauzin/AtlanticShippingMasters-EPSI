1ce64f6b4a3fa19f3bc67ad2d006f803486c77ec
louis <louis.escartin@hotmail.fr>
2020-11-09 15:19:52 +0100

    added algorithm to find shortest path

--- cpp/Parcours/Parcours.cpp
+++ cpp/Parcours/Parcours.cpp
@@ -1,20 +1,117 @@
-// Parcours.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <typeinfo>
+#include <algorithm>
+using namespace std;
+
+vector<string> fullDestinations;
+vector<int> totalDistanceOfEachPath;
+
+void findShortestPath(string str, int strBegin, int strEnd, string startingPoint)
+{
+    // je simule les distances entre les différents ports le temps de faire le C#
+    map<string, int> distances;
+    distances["DA"] = 5;
+    distances["DB"] = 3;
+    distances["DC"] = 4;
+    distances["AB"] = 7;
+    distances["AC"] = 8;
+    distances["BA"] = 7;
+    distances["BC"] = 12;
+    distances["CB"] = 12;
+    distances["CA"] = 8;
+    distances["CD"] = 4;
+    distances["BD"] = 3;
+    distances["AD"] = 5;
+
+    // map<string, int>::iterator it;
+
+    if (strBegin == strEnd)
+    {
+        string mystr = startingPoint + str + startingPoint;
+        int begin = 0;
+        int end = begin + 1;
+        string couple = "";
+        int totalDistanceForcurrentCouple = 0;
+
+        // cout << "\n";
+        // cout << mystr << endl;
+
+        for (int j = 0; j <= mystr.size(); j++)
+        {
+            couple = couple + mystr[begin] + mystr[end];
+            begin++;
+            end++;
+            auto it = distances.find(couple);
+            if (begin == mystr.size())
+            {
+                break;
+            }
+            // cout << couple << endl;
+            couple = "";
+            totalDistanceForcurrentCouple += it->second;
+        }
+        cout << totalDistanceForcurrentCouple << endl;
+        totalDistanceOfEachPath.push_back(totalDistanceForcurrentCouple);
+        fullDestinations.push_back(mystr);
+    }
+    else
+    {
+
+        for (int i = strBegin; i <= strEnd; i++)
+        {
+
+            swap(str[strBegin], str[i]);
+
+            findShortestPath(str, strBegin + 1, strEnd, startingPoint);
+
+            swap(str[strBegin], str[i]);
+        }
+    }
+}
+
+void getIndex(vector<int> v, int K)
+{
+    vector<int>::iterator it = find(v.begin(), v.end(), K);
+    if (it != v.end())
+    {
+
+        int index = distance(v.begin(), it);
+        cout << "Le chemin le plus court est " << fullDestinations[index] << endl;
+    }
+    else
+    {
+        cout << "Il est mauvais ton algo Jack! C'est la piquette Jack!" << endl;
+    }
+}
+
+// pour l'exemple je suppose qu'on a des ports A,B,C,D et qu'il faut partir et revenir du port D en passant par A,B et C en trouvant le plus court chemin
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string necessaryPorts = "ABC";
+    int length = necessaryPorts.size();
+    findShortestPath(necessaryPorts, 0, length - 1, "D");
+
+    for (auto path : fullDestinations)
+    {
+        cout << path << endl;
+        cout << "\n";
+    }
+
+    int shortestDistance = 999;
+
+    for (auto distance : totalDistanceOfEachPath)
+    {
+        if (distance < shortestDistance)
+        {
+            shortestDistance = distance;
+        }
+    }
+
+    getIndex(totalDistanceOfEachPath, shortestDistance);
+
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
